323a8db22643e53c21221184cb4873aba7ed8510
PriyanJindal <52796578+PriyanJindal@users.noreply.github.com>
2020-08-14 13:00:43 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+public static void main(String[ ] args)
+{
+    System.out.println("Hello World") ;
+    System.out.println("My name is Priyan Jindal.");
+    System.out.println("I love playing basketball.");
+    System.out.println("My favorite type of food is Mexican.") ;
+    System.out.println("I want to study math/econ or CS/econ in college.") ;
+    System.out.println("I would love to go to UCLA.") ;
+    
+}
+}
